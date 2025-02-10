9c08368061b5f6f888523a73dd09a5d1e731bb2a
JamesonWasHere <inm3102+@gmail.com>
2025-02-10 07:52:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://proxyium.com/';
+const nggUrl = 'https://www.y9freegames.com/game/roblox-online/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
